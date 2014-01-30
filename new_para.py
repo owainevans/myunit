b1e5502b5e7ab36a907039f6aa40c8b3901f0a04
Owain Evans <owain@mit.edu>
2014-01-30 17:11:14 -0500

    added probes and total_transitions attribute. need to output total transitions somewhere or see if total trans is recorded by a ripl

--- new_para.py
+++ new_para.py
@@ -9,7 +9,9 @@ worker_state = {'init':False}
 
 
 def worker(st_func_args):
-    if not(worker_state['init']):
+    ## ripl.clear() resets seed and would set all workers to same seed
+    # so we check for a reset with list_directives
+    if not(worker_state['init']) or ripl.list_directives()==[]:
         p_name = multiprocessing.current_process().name
         name = 'w'+ p_name[p_name.rfind('-')+1:]
         print 'Init: ', name,'.'
@@ -30,7 +32,13 @@ class MultiRipl():
     def __init__(self,no_ripls):
         self.pool = Pool(processes = no_ripls)
         self.no_ripls = no_ripls
+        self.total_transitions = 0
+
+        ## FIXME maybe clear ripl on init
         print 'New MultiRipl with %i ripls' % self.no_ripls
+    
+    def clear(self):
+        return self.delegate('ripl.clear()')
 
     def observe(self,exp,value):
         st =  'ripl.observe(" %s ", " %s " )' % (str(exp), str(value) )
@@ -49,6 +57,7 @@ class MultiRipl():
         return self.delegate(st)
 
     def infer(self,no_transitions):
+        self.total_transitions += no_transitions
         st = 'ripl.infer(%i)' % no_transitions
         return self.delegate(st)
 
@@ -63,7 +72,6 @@ class MultiRipl():
         st = 'ripl.execute_program(" %s ")' % prog
         return self.delegate(st)
 
-        
     def s(self,st): return self.delegate("ripl."+st)
 
     def delegate(self,st):
@@ -86,11 +94,62 @@ class MultiRipl():
         plt.scatter(vals1,vals2)
         plt.show()
 
+    def probes(self,label,no_transitions,no_probes,plot=False):
+        step = int(round (float(no_transitions+1) / no_probes) )
+        probes = range(0,no_transitions+step,step)
+
+        series = [];
+        for probe in probes:
+            self.infer(step)
+            series.append( self.snapshot(label) )
+            
+        if plot=='hist':
+            no_probes = len(probes)
+            xmin = min([min(shot) for shot in series])
+            xmax = max([max(shot) for shot in series])
+            fig,ax = plt.subplots(ncols=no_probes,sharex=True,figsize=(12,5) )
+            for i in range(no_probes):
+                ax[i].hist(series[i],bins=20)
+                ax[i].set_xlim([xmin,xmax])
+                ax[i].set_title('Probe at %i out of %i' % (probes[i],no_transitions+step))
+                
+            fig.tight_layout()
+
+        if plot=='series':
+            fig,ax = plt.subplots()
+            for ripl in range(self.no_ripls):
+                vals = [shot[ripl] for shot in series]
+                ax.plot(probes,vals,label='R'+str(ripl))
+            ax.legend()
+
+        return probes,series
+        
     def terminate(self):
         self.pool.terminate()
         print 'terminated all ripl processes'
         
         
+def wm(no_ripls):
+    v = MultiRipl(no_ripls)
+    v.clear()
+    v.assume('r','(normal 0 30)')
+    v.assume('s','(beta 1 1)')
+    v.assume('w','(normal r 6.)')
+    v.observe('w','50.')
+    v.assume('w2','(normal r 6.)')
+    v.observe('w2','50.')
+   
+    #v.infer(1)
+    return v
+
+
+
+
+
+
+
+
+
 # my = MultiRipl(10)
 # a=[0]*10
 # a[0] = my.assume("mu","(normal 0 20)",label='mu');
@@ -100,43 +159,31 @@ class MultiRipl():
 # a[4] = my.infer(400)
 # a[5] = my.report('mu')
 
-sprinkler = '''
-[clear]
-[assume rain (bernoulli .1) ]
-[assume spr (bernoulli .4) ]
-[assume wet (if (* rain spr)
-                 .9
-                 (if spr .7 (if rain .5 .05) ) ) ]'''
+# sprinkler = '''
+# [clear]
+# [assume rain (bernoulli .1) ]
+# [assume spr (bernoulli .4) ]
+# [assume wet (if (* rain spr)
+#                  .9
+#                  (if spr .7 (if rain .5 .05) ) ) ]'''
 
-spr = '''
-[clear]
-[assume rain (beta 1 1)]
-[assume spr (beta 1 1) ]
-[assume wet (lambda () (if (flip rain) true
-                            (if (flip spr) true (flip .1) ) ) )]
-'''
-spr = ' '.join(spr.split('\n'))
+# spr = '''
+# [clear]
+# [assume rain (beta 1 1)]
+# [assume spr (beta 1 1) ]
+# [assume wet (lambda () (if (flip rain) true
+#                             (if (flip spr) true (flip .1) ) ) )]
+# '''
+# spr = ' '.join(spr.split('\n'))
 
-spr = '''
-[clear]
-[assume rain (beta 1 1)]
-[assume spr (beta 1 1) ]
-[assume w1 (normal (+ rain spr) .1) ]
-[observe w1 1.3]
+# spr = '''
+# [clear]
+# [assume rain (beta 1 1)]
+# [assume spr (beta 1 1) ]
+# [assume w1 (normal (+ rain spr) .1) ]
+# [observe w1 1.3]
 
-'''
-
-v = MultiRipl(40)
-v.assume('r','(beta 1 1)')
-v.assume('s','(beta 1 1)')
-v.assume('w','(normal (+ r s) .1)')
-v.observe('w','1.3')
-v.infer(100)
-
-
-
-
-
+# '''
 
 
 
