d0e7247a2ed489908485b19775b0c33e8a833b50
Owain Evans <owain@mit.edu>
2014-03-07 14:34:40 -0500

    vari

--- mm_utils.py
+++ mm_utils.py
@@ -9,45 +9,88 @@ uniform = np.random.uniform
 dirichlet = np.random.dirichlet
 multinoulli = lambda dist,size: np.argmax(multinomial(1,dist,size),axis=1)
 
+def rep_with(text,dic):
+    for i, j in dic.iteritems():
+        text = text.replace(i, j)
+    return text
+
+mix_model='''
+
+
+
+'''
+
 crp_model='''
-[assume alpha (uniform_continuous .01 1)]
+[assume alpha (uniform_continuous <<prior_alpha>>)]
 [assume crp (make_crp alpha) ]
 [assume z (mem (lambda (i) (crp) ) ) ]
-[assume mu (mem (lambda (z dim) (normal 0 5) ) ) ] 
-[assume sig (mem (lambda (z dim) (uniform_continuous .1 8) ) ) ]
+
+[assume mu_loc <<prior_mu_loc>>]
+[assume mu_scale <<prior_mu_scale>>]
+[assume sigma_scale <<prior_sigma_scale>>]
+
+[assume mu (mem (lambda (z dim) (normal mu_loc mu_scale) ) ) ] 
+[assume sigma (mem (lambda (z dim) (gamma sigma_scale 1) ) )]
 [assume x_d (lambda (dim)
-             (  (lambda (z) (normal (mu z dim) (sig z dim)))
+             (  (lambda (z) (normal (mu z dim) (sigma z dim)))
                  (crp)  ) ) ]
-[assume x (mem (lambda (i dim) (normal (mu (z i) dim) (sig (z i) dim)))  ) ]
+[assume x (mem (lambda (i dim) (normal (mu (z i) dim) (sigma (z i) dim)))  ) ]
 
 [assume n (gamma 1 1)]
 [assume model_name (quote crp)]
 '''
+inf_vars={'<<prior_alpha>>':'.01 1',
+          '<<prior_mu_loc>>':'(normal 0 20)',
+          '<<prior_mu_scale>>':'(gamma 5 1)',
+          '<<prior_sigma_scale>>':'(uniform_continuous .5 10)'}
+zeros_vars={'<<prior_alpha>>':'.01 .05',
+          '<<prior_mu_loc>>':'0',
+          '<<prior_mu_scale>>':'1',
+          '<<prior_sigma_scale>>':'1'}
 
+inf_crp_model = rep_with(crp_model,inf_vars)
+zeros_crp_model= rep_with(crp_model,zeros_vars)
 
 def test_crp():
-    v=mk_l(); v.execute_program(crp_model)
-    v.observe('alpha','.001')
-    v.observe('(mu 0 0)','0')
-    v.observe('(mu 0 1)','0')
-    v.observe('(sig 0 0)','.01')
-    v.observe('(sig 0 1)','.01')
-    v.infer(500)
-    xys=[v.predict( '(list (x %i 0) (x %i 1))' %(i,i)) for i in range(30) ]
-    print xys
+    #v=mk_c(); v.execute_program(inf_crp_model)
+    #no_xs = 30
+    ## FIXME: why doesn't this work?
+    # v.observe('alpha','.001')
+    # v.observe('(mu 0 0)','0')
+    # v.observe('(mu 0 1)','0')
+    # v.observe('(sig 0 0)','.01')
+    # v.observe('(sig 0 1)','.01')
+    # v.infer(1)
+    # xys=[v.predict( '(list (x %i 0) (x %i 1))' %(i,i)) for i in range(no_xs) ]
+    # print xys
     #assert .1 > abs( np.mean( if_lst_flatten(xys)) )
-    v=mk_c(); v.execute_program(crp_model)
+    no_xs = 30
+    xys = np.random.normal(0,.01,size=(no_xs,2) )
+    print xys
+    vinf=mk_c(); vinf.execute_program(inf_crp_model)
+    vzeros=mk_c(); vzeros.execute_program(zeros_crp_model)
+    vs=[vinf,vzeros]
+    for v in vs:
+        v.sample('(x 0 0)')
+        print [v.predict( '(list (x %i 0) (x %i 1))'%(i,i) ) for i in range(20) ]
     for i,(x0,x1) in enumerate(xys):
-        #v.observe('(list (x %i 0) (x %i 1))'%(i,i),'(list %f %f)'%(x0,x1) )
-        v.observe('(x %i 0)'%i,'%f'%x0);v.observe('(x %i 1)'%i,'%f'%x1)
-        v.observe('(z %i)'%i,'%i'%0)
-    mus=[],sigs=[]
-    for r in range(5):
-        v.infer(800)
-        mus.append( v.sample('(list (mu 0 0) (mu 0 1))') )
-        sigs.append( v.sample('(list (sig 0 0) (sig 0 1))') )
-    print mus,sigs
+        [v.observe('(x %i 0)'%i,'%f'%x0) for v in vs]
+        [v.observe('(x %i 1)'%i,'%f'%x1) for v in vs]
+        #v.observe('(z %i)'%i,'%i'%0)
+
+    [v.infer(5000) for v in vs]    
+    loops=3; mus=[[]]*loops; sigs=[[]]*loops
+    for i,v in enumerate(vs):
+        for r in range(loops):
+            v.infer(2500)
+            zvals = np.unique([ v.sample('(z %i)' %i) for i in range(no_xs)] )
+            print zvals
+            mus[r]=[ (z,v.sample('(list (mu %i 0) (mu %i 1))'%(z,z)) ) for z in zvals]
+            sigs[r]=[(z,v.sample('(list (sigma %i 0) (sigma %i 1))'%(z,z) )) for z in zvals]
+        s= 'vinf' if i==0 else 'vzeros'
+        print s, '-- mus:',mus, '\n sig:',sigs
     
+
 
 def generate_data_mm(n,k=3,model='gauss'):
     dim=2
