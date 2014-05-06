a054d7720cb3f7580297baadfa1b5f5a38c44a22
Owain Evans <owain@mit.edu>
2014-05-06 19:40:57 -0400

    hymm

--- unit/myhmm.py
+++ unit/myhmm.py
@@ -1,6 +1,6 @@
-from venture import shortcuts
 from venture.venturemagics.ip_parallel import *
-from analytics import *
+from venture.unit import *
+execfile('/home/owainevans/Venturecxx/python/lib/unit/history.py')
 
 xs = [4.17811131241034, 3.8207451562269097, 3.8695630629179485,
       0.22006118284977338, 2.210199033799397, 3.783908156611711,
@@ -11,18 +11,7 @@ xs = [4.17811131241034, 3.8207451562269097, 3.8695630629179485,
       2.096957795256201, 3.962559707705782, 2.0649737290837695,
       4.447773338208195, 3.0441473773992254, 1.9403530443371844,
       2.149892339815339, 1.8535027835799924, 1.3764327100611682,
-      2.787737100652772, 4.605218953213757, 4.3600668534442955,
-      4.479476152575004, 2.903384365135718, 3.228308841685054,
-      2.768731834655059, 2.677169426912596, 4.548729323863021,
-      4.45470931661095, 2.2756630109749754, 3.8043219817661464,
-      4.041893001861111, 4.932539777501281, 3.392272043248744,
-      3.5285486875160186, 1.7961542635140841, 2.9493126820691664,
-      1.7582718429078779, 3.444330463983401, 2.031284816908312,
-      1.6347773147087383, 4.423285189276542, 0.5149704854992727,
-      4.470589149104097, 4.4519204418264575, 3.610788527431577,
-      3.7908243330830036, 3.0038367596454187, 3.3486671878130028,
-      4.474091346599369, 2.7734106792197633, 1.8127987198750086]
-
+      2.787737100652772, 4.605218953213757, 4.3600668534442955,]
 xs[3:7] = [-x for x in xs[5:10]]
 xs[17:23] = [-x for x in xs[17:23]]
 
@@ -76,28 +65,11 @@ ar1Model = """
   (lambda (state)
     (normal (pow (- state 5) 2) observation_noise))]
 """
-ys = [5,5,5,5,5,.1,.1]
+
 # note: we could follow robert. allow states 0 and 2 to be observed
 # and then get the observation for state 1.the negative point is favored
 # coz the other observed states are negative and noise terms (which are known)
 # are very high
-ar1Observes=[("(observation_fn (get_state %d))"%t, y) for t,y in enumerate(ys)]
-ar1Program=(ar1Model,ar1Observes)
-
-def anaLoad(prog=ar1Program,ripl='p',length=5):
-  v = mk_p_ripl() if ripl=='p' else mk_l_ripl()
-  model=prog[0]
-  observes=prog[1][:length]
-  v.execute_program(model)
-  [v.observe(exp,literal) for exp,literal in observes]
-  queryExps=['(get_state %d)' % t for t,_ in enumerate(observes)]
-  ana=Analytics(v,queryExps=queryExps)
-  return ana
-  
-inf=["(mh default one 1)",
-     "(cycle ((mh hypers one 3) (mh state one 10)) 1)",
-     "(cycle ((mh hypers one 3) (pgibbs state ordered 6 1)) 1)",
-     "(cycle ((mh hypers one 3) (func-pgibbs state ordered 12 1)) 1)"]
 
 # 'default' is scope containing all random choices (latent is random choices
 # hidden from venture.
@@ -110,35 +82,130 @@ inf=["(mh default one 1)",
 # simultaneous particules to represent alternative possibilities. 
 # composition: (cycle (<inf_exp> ... <#transitions>), (mixture ((<w1> <exp1>) ...)
 
-length=3
-phmm = anaLoad(prog=hmmProgram,length=length)
-par =  anaLoad(prog=ar1Program,length=length)
-ana = par
-h0,r0 = ana.runFromConditional(10,runs=1,infer=inf[0])
-h1,r1 = ana.runFromConditional(10,runs=1,infer=inf[1])
-h2,r2 = ana.runFromConditional(20,runs=1,infer=inf[2])
-h3,r3 = ana.runFromConditional(10,runs=1,infer=inf[3])
-hs=[h0,h1,h2,h3]
 
+ys = [5,5,5,.1,.1]
+ar1Observes=[("(observation_fn (get_state %d))"%t, y) for t,y in enumerate(ys)]
+ar1Program=(ar1Model,ar1Observes)
+
+
+def loadTSModel(program=ar1Program, backend='puma', observesLength=3):
+  assumes=program[0]
+  observes=program[1][:observesLength]
+
+  # load ripl with program
+  v = mk_p_ripl() if backend=='puma' else mk_l_ripl()
+  v.execute_program(assumes)
+  [v.observe(exp,literal) for exp,literal in observes]
+  queryExps=['(get_state %d)' % t for t,_ in enumerate(observes)]
+  return Analytics(v,queryExps=queryExps)
+  
+
+inferProgs=["(mh default one 1)",
+            "(cycle ((mh hypers one 3) (mh state one 10)) 1)",
+            "(cycle ((mh hypers one 3) (pgibbs state ordered 6 1)) 1)",
+            "(cycle ((mh hypers one 3) (func-pgibbs state ordered 12 1)) 1)"]
+
+inferProgsParams = [ (200,1), (100,1), (20,1), (200,1) ]
+inferProgsParams = zip([5]*4,[1]*4)
+observesLength=3
+model = loadTSModel( program=ar1Program, observesLength=observesLength)
+
+def runInferProgs():
+  histories = []
+  for inferProg,inferParams in zip(inferProgs,inferProgsParams):
+    print 'Infer Prog: %s'%inferProg
+    sweeps,runs = inferParams
+    history,ripl = model.runFromConditional(sweeps, runs=runs, infer=inferProg,
+                                            name=inferProg)
+    histories.append(history)
+  return histories
+
+histories = runInferProgs()
+histories[3].quickHistogram('(get_state 0)')
+[h.quickPlot('(get_state 0)') for h in histories]
+[h.quickPlot('observation_noise') for h in histories]
+[h.quickScatter('(get_state 0)','(get_state 1)') for h in histories]
+
+
+historyOV=historyOverlay('MH vs. PGibbs',
+                         (('MH',histories[0]),('PG',histories[3])))
+historyOV.quickPlot('(get_state 0)')
+
+
+def runFromJointDiagnostic(inferProgs,sweeps=100):
+  hists = []
+  for inferProg in inferProgs:
+    hists.append(model.runFromJoint( sweeps, runs=1,
+                                    infer=inferProg,name=inferProg))
+  hists.append(model.sampleFromJoint(sweeps))
+  return hists
+
+
+fromJointHistories = runFromJointDiagnostic((inferProgs[0],inferProgs[3]), sweeps=200)
+fromJointPairs = zip(['MH','PG','Fwd'],fromJointHistories)
+fromJointOV=historyOverlay('MH vs. PGibbs vs. Forward',fromJointPairs)
+fromJointOV.quickHistogram('(get_state 0)')
+fromJointOV.quickPlot('(get_state 0)')
+
+# Inference Diagnostic: testFromPrior
+# The initial state *(get_state 0)* is drawn from N(0,3.5). We generate synthetic datasets from the prior and run inference. We take one sample from the approximate posterior (i.e. one sample per dataset). These samples would have distribution N(0,3.5) were the approximation perfect. We can diagnose problems with inference by comparing samples from the prior to these samples due to inference.  
+
+
+def testFromPrior(noDatasets,sweeps=100,plotLimit=10,inferProg=None):
+  if inferProg is None: 
+    inferProg=inferProgs[0]
+    
+  datasetToHistory = []
+  getStateList = []
+  
+  for dataset in range(noDatasets):
+    history,_ = model.runConditionedFromPrior(sweeps,runs=1,infer=inferProg,
+                                              name='Dataset %i'%dataset)
+
+    getState = history.nameToSeries['(get_state 0)'][0].values[-1]
+    print 'Dataset %i: last sample of (get_state 0) = %.2f'%(dataset,getState)
+    getStateList.append(getState)
+
+    if dataset < plotLimit: history.quickPlot('(get_state 0)')
+
+    datasetToHistory.append( history )
+
+  priorSamples = np.random.normal(0,3.5,noDatasets)
+  fig,ax = plt.subplots(figsize=(10,4))
+  ax.hist(getStateList, color='r', alpha=.6,
+          label='Inferred (get_state 0) across datasets')
+  ax.hist(priorSamples,color='y',alpha=.6,
+          label='Samples from N(0,3.5) prior on (get_state 0)')
+  ax.set_title('Inferred vs. Prior Samples for (get_state 0)')
+  print 'getstate (across datasets): (mean, std) '
+  print np.mean(getStateList),np.std(getStateList)
+  print 'getstate (forward sample from prior): (mean, std) '
+  print np.mean(priorSamples),np.std(priorSamples)
+  
+  return datasetToHistory,fig
+
+noDatasets=5
+datasetToHistory,fig = testFromPrior(noDatasets,sweeps=10)
+testFromPairs = zip( map(str,range(noDatasets)), datasetToHistory)
+testFromPriorOV = historyOverlay('testFromPrior',testFromPairs[:5])
+                              
 
 # want to actually see how good the second mode is? we can force and 
 # look at logscore. but with enough particles, we should get it.
 def sampStates(r):
-  s=[r.sample('(get_state %d)'%t) for t in range(length)]
+  s=[r.sample('(get_state %d)'%t) for t in range(observesLength)]
   return r.get_global_logscore(),s
 
 def forcer(r):
   print 'forcer'
   print 'samp:',sampStates(r)
-  [r.force('(get_state %d)'%t,'7') for t in range(length)]
+  [r.force('(get_state %d)'%t,'7') for t in range(observesLength)]
   print 'samp:',sampStates(r)
   r.infer(1)
   print 'after infer'
   print 'samp:',sampStates(r)
   
-
-# do diagnostics work? we can try running runFromJoint, and 
-# see if that differentiates the two inference progs. our problem
+# our problem
 # here is autocorrelation, not convergence to staionarity/equilib.
 # i expect that geweke will move around a fair amount. we 
 # could also try running on multi datasets. what to see? we should 
@@ -147,33 +214,34 @@ def forcer(r):
 # the model won't find it. so it won't respect prior. 
 
 # geweke
-#geh = ana.runFromJoint(100,runs=1,infer=inf[0])
 
-noDatasets=2
-hists2=[]
-exps=['(get_state %d)'%t for t in range(length)]
-gt={exp:[] for exp in exps}
-s=gt.copy()
-sweeps=100
-probes=range(int(.5*sweeps),sweeps,10)
-
-for i in range(noDatasets):
-  print 'dataset: ',i
-  h2,_=ana.runConditionedFromPrior(sweeps,runs=1,infer=inf[0])
-  hists2.append(h2)
-  
-  for exp in exps:
-    gt[exp].append(  h2.groundTruth[exp]['value'] )
-    s[exp].extend( [h2.nameToSeries[exp][0].values[p] for p in probes] )
-
-# sample from joint
-fwdh = ana.sampleFromJoint(len(s[exps[0]]))
-
-fwd={exp:fwdh.nameToSeries[exp][0].values for exp in exps}
 
 from ge import *
-stats_dict,fig = compareSampleDicts([s,fwd],['test2_inf','fwd'],plot=True)
 
+def test2(noDatasets=2):
+  hists2=[]
+  exps=['(get_state %d)'%t for t in range(observesLength)]
+  gt={exp:[] for exp in exps}
+  s=gt.copy()
+  sweeps=200
+  probes=range(int(.5*sweeps),sweeps,10)
+
+  for i in range(noDatasets):
+    print 'dataset: ',i
+    h2,_=model.runConditionedFromPrior(sweeps,runs=1,infer=inf[0])
+    hists2.append(h2)
+
+    for exp in exps:
+      gt[exp].append(  h2.groundTruth[exp]['value'] )
+      s[exp].extend( [h2.nameToSeries[exp][0].values[p] for p in probes] )
+
+  # sample from joint
+  fwdh = model.sampleFromJoint(len(s[exps[0]]))
+
+  fwd={exp:fwdh.nameToSeries[exp][0].values for exp in exps}
+
+  stats_dict,fig = compareSampleDicts([s,fwd],['test2_inf','fwd'],plot=True)
+  return hist2,s,fwd,stats_dict
 
 
 
